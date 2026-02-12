842df771f8eef4c16e8d6e8dfd97941d636aef57
Paulo Ranieri dos Santos <87606779+Ranieriss@users.noreply.github.com>
2026-02-12 11:09:46 -0300

    Fix supabaseAnonKey formatting in config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const supabaseUrl = "https://gcmhgyjjinqafbdmuaqy.supabase.co";
-export const supabaseAnonKey = "COLE_AQUI_A_CHAVE_sb_publishable_...";
+export const supabaseUrl = "https://gcmhgyjjinqafbdmuaqy.supabase.co;
+export const supabaseAnonKey = "sb_publishable_JKtypI8V_ussH5TRL0Rqng_tU324Xk-";
